a15d6595f6c1f3d1529782b6e4fd4ebf9b9278a5
skyerzz <spammen67@gmail.com>
2019-11-03 19:11:42 +0100

    started spider movements & tests

--- hive/src/main/java/hive/movement/MoveSpider.java
+++ hive/src/main/java/hive/movement/MoveSpider.java
@@ -1,4 +1,11 @@
 package hive.movement;
 
+import hive.Board;
+import hive.Coordinate;
+
 public class MoveSpider {
+
+    public static boolean canSpiderMove(Board board, Coordinate from, Coordinate to){
+        return true;
+    }
 }
--- hive/src/test/java/hive/move/SpiderMoveTest.java
+++ hive/src/test/java/hive/move/SpiderMoveTest.java
@@ -1,4 +1,22 @@
 package hive.move;
 
+import hive.Board;
+import hive.Coordinate;
+import hive.movement.MoveSpider;
+import nl.hanze.hive.Hive;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SpiderMoveTest {
+
+    @Test
+    void spiderSingleSurroundment(){
+        Board board = new Board();
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.QUEEN_BEE, new Coordinate(0, 0));
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.SPIDER, new Coordinate(1, 0));
+
+
+        assertTrue(MoveSpider.canSpiderMove(board, new Coordinate(0, 0), new Coordinate(2,0)));
+    }
 }
