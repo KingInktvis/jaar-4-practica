db854fcf83544e57c429823094752c69639bc2f6
skyerzz <spammen67@gmail.com>
2019-11-03 19:11:30 +0100

    added queen movements & tests

--- hive/src/main/java/hive/movement/MoveQueen.java
+++ hive/src/main/java/hive/movement/MoveQueen.java
@@ -1,4 +1,13 @@
 package hive.movement;
 
+import hive.Board;
+import hive.Coordinate;
+
 public class MoveQueen {
+
+    public static boolean canQueenMove(Board board, Coordinate from, Coordinate to){
+        boolean move = MoveCommon.getCoordinatesInRange(board, from, 1, false).contains(to);
+        return move;
+    }
+
 }
--- hive/src/test/java/hive/move/QueenMoveTest.java
+++ hive/src/test/java/hive/move/QueenMoveTest.java
@@ -1,4 +1,49 @@
 package hive.move;
 
+import hive.Board;
+import hive.Coordinate;
+import hive.Move;
+import hive.movement.MoveBeetle;
+import hive.movement.MoveQueen;
+import nl.hanze.hive.Hive;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class QueenMoveTest {
+
+    @Test
+    void QueenMoveEmptySurroundings(){
+        Board board = new Board();
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.SOLDIER_ANT, new Coordinate(0, 0));
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.QUEEN_BEE, new Coordinate(1, 0));
+        assertTrue(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(0,1)));
+        assertTrue(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(1,-1)));
+
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(-1,1)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(0,-1)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(1,0)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(0,0)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(2,0)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(0,-2)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(1,-2)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(-1,2)));
+    }
+
+    @Test
+    void QueenMoveBlockedOption(){
+        Board board = new Board();
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.QUEEN_BEE, new Coordinate(0, 0));
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.BEETLE, new Coordinate(1, 0));
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.BEETLE, new Coordinate(0, 1));
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.BEETLE, new Coordinate(-1, 0));
+
+        assertTrue(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(0,-1)));
+        assertTrue(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(1,-1)));
+
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(0, 0)));
+        assertFalse(MoveQueen.canQueenMove(board, new Coordinate(0, 0), new Coordinate(-1,1)));
+
+    }
 }
