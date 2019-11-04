24f795dc216018d1d3517a81927dde72cf2477c4
Sky <Sky@Laptop-R>
2019-11-04 15:18:58 +0100

    updated and improved spider tests

--- hive/src/test/java/hive/move/SpiderMoveTest.java
+++ hive/src/test/java/hive/move/SpiderMoveTest.java
@@ -24,6 +24,20 @@ public class SpiderMoveTest {
     @Test
     void spiderDoubleSurroundment(){
 
+        Board board = new Board();
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.QUEEN_BEE, new Coordinate(0, 0));
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.QUEEN_BEE, new Coordinate(-1, 0));
+        board.placeTile(Hive.Player.BLACK, Hive.Tile.SPIDER, new Coordinate(1, 0));
+
+        assertTrue(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(-1,-1)));
+        assertTrue(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(-2,1)));
+        assertFalse(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(0,1)));
+        assertFalse(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(1,-1)));
+        assertFalse(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(0,-1)));
+        assertFalse(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(-1,1)));
+        assertFalse(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(-2,0)));
+        assertFalse(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(-1,0)));
+        assertFalse(MoveSpider.canSpiderMove(board, new Coordinate(1, 0), new Coordinate(0,0)));
     }
 
     @Test
