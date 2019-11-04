dd7157269c3d61167b81e0acb2418b99059eb005
Sky <Sky@Laptop-R>
2019-11-04 15:18:46 +0100

    small change in total tiles getter

--- hive/src/main/java/hive/Coordinate.java
+++ hive/src/main/java/hive/Coordinate.java
@@ -12,11 +12,11 @@ public class Coordinate {
         this.r = r;
     }
 
-    int getQ() {
+    public int getQ() {
         return q;
     }
 
-    int getR() {
+    public int getR() {
         return r;
     }
 
